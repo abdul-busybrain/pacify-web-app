f72103bee5fe67326699e8f1f1fd1093e18a4b5e
abdul-busybrain <awwalhajine@gmail.com>
2025-01-27 12:05:47 +0100

    Started Services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,60 @@
 "use client";
 
-import { Button, ConfigProvider,  } from "antd";
+import { Button, ConfigProvider } from "antd";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
+  const pathname = usePathname();
+
   return (
     <ConfigProvider>
-      {/* Header Section */}
-      <header className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center py-4 px-4 md:px-8 bg-transparent ml-4 mr-4">
-        <Link href={"/"}>
+      {/* HEADER SECTION */}
+      <header className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center py-4 px-4 md:px-8 bg-transparent ml-4 mr-4 rounded-t-[50px]">
+        <Link href={"/"} className="rounded-full bg-white p-4">
           <Image src={"/images/logo.png"} height={120} width={120} alt="Logo" />
         </Link>
 
         <nav>
-          <ul className="flex gap-8">
-            <li>
-              <Link
-                href="/"
-                className="text-white hover:text-gray-300 transition-colors"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={"#"}
-                className="text-white hover:text-gray-300 transition-colors"
-              >
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={"#"}
-                className="text-white hover:text-gray-300 transition-colors"
-              >
-                Products
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={"#"}
-                className="text-white hover:text-gray-300 transition-colors"
-              >
-                FAQ
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={"#"}
-                className="text-white hover:text-gray-300 transition-colors"
-              >
-                Blog
-              </Link>
-            </li>
+          <ul className="flex gap-8 rounded-full bg-purple-500/50 p-4">
+            {[
+              { href: "/", label: "Home" },
+              { href: "/services", label: "Services" },
+              { href: "/products", label: "Products" },
+              { href: "/faq", label: "FAQ" },
+              { href: "/blog", label: "Blog" },
+            ].map(({ href, label }) => (
+              <li key={href}>
+                <Link
+                  href={href}
+                  className={`text-white transition-colors rounded-full px-2 py-2 ${
+                    pathname === href
+                      ? "bg-white text-purple-500 "
+                      : "hover:bg-white hover:text-purple-500"
+                  }`}
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
 
         <Link href={"#"}>
-          <Button>Contact Us</Button>
+          <Button
+            size="large"
+            className="rounded-full p-4 text-purple-500 font-bold text-lg"
+          >
+            Contact Us
+          </Button>
         </Link>
       </header>
 
       <main>
-        {/* Hero Section */}
+        {/* HERO SECTION */}
         <section
-          className="relative bg-cover h-screen -mx-4 md:-mx-8"
+          className="relative bg-cover h-screen -mx-4 md:-mx-8 rounded-t-[50px] rounded-b-[50px]  overflow-hidden"
           style={{ backgroundImage: "url(/images/hero.jpg)" }}
         >
           <div className="absolute inset-0 bg-black/50 backdrop-blur-sm">
@@ -78,12 +66,18 @@ export default function Home() {
                 We offer outstanding web, mobile, and digital services suited to
                 your needs.
               </p>
-              <Button size="large" className="px-8 w-max">
+              <Button
+                size="large"
+                className="px-8 w-max rounded-full p-4 bg-purple-500 text-white font-bold text-lg hover:bg-purple-300"
+              >
                 Get a Quote
               </Button>
             </div>
           </div>
         </section>
+
+        {/* SERVICES SECTION */}
+        <section></section>
       </main>
       <footer></footer>
     </ConfigProvider>
